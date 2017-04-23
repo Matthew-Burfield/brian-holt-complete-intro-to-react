c872c07fba26cb99736c3edb6c21a6365583823b
Matthew-Burfield <m1.burfield@gmail.com>
2017-04-23 21:11:34 +1000

    styled show data a little bit better

--- js/Search.js
+++ js/Search.js
@@ -5,7 +5,16 @@ const Search = React.createClass({
   render () {
     return (
       <div className='search'>
-        {preload.shows.map(show => <h3>{show.title}</h3>)}
+        {preload.shows.map(show =>
+          <div className='show-card'>
+            <img src={`/public/img/posters/${show.poster}`} />
+            <div>
+              <h3>{show.title}</h3>
+              <h4>({show.year})</h4>
+              <p>{show.description}</p>
+            </div>
+          </div>
+        )}
       </div>
     )
   }
