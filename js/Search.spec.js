0510657bc82e8322ee9e9c463cf45a111a04c2a4
Matthew-Burfield <m1.burfield@gmail.com>
2017-04-24 00:34:38 +1000

    Added final test to test code covereage

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -15,3 +15,17 @@ test('Search should render a ShowCard for each show', () => {
   const component = shallow(<Search />)
   expect(component.find(ShowCard).length).toEqual(preload.shows.length)
 })
+
+test('Search should render correct amount of shows based on search', () => {
+  const searchWord = 'house'
+  const component = shallow(<Search />)
+  const showCount = preload.shows.filter(show => {
+    return (
+      `${show.title} ${show.description}`
+        .toUpperCase()
+        .indexOf(searchWord.toUpperCase()) >= 0
+    )
+  }).length
+  component.find('input').simulate('change', {target: {value: searchWord}})
+  expect(component.find(ShowCard).length).toEqual(showCount)
+})
