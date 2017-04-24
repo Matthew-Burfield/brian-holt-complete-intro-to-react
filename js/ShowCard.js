080ca5bbdad6329aa486920e17bb885a3c8fffbc
Matthew-Burfield <m1.burfield@gmail.com>
2017-04-24 10:59:25 +1000

    Added router Link to ShowCard to route to Details page

--- js/ShowCard.js
+++ js/ShowCard.js
@@ -1,24 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router'
 const { string } = React.PropTypes
 
 const ShowCard = React.createClass({
   propTypes: {
+    imdbID: string,
     poster: string,
     title: string,
     year: string,
     description: string
   },
   render () {
-    const { poster, title, year, description } = this.props
+    const { imdbID, poster, title, year, description } = this.props
     return (
-      <div className='show-card'>
-        <img src={`/public/img/posters/${poster}`} />
-        <div>
-          <h3>{title}</h3>
-          <h4>({year})</h4>
-          <p>{description}</p>
+      <Link to={`/Details/${imdbID}`}>
+        <div className='show-card'>
+          <img src={`/public/img/posters/${poster}`} />
+          <div>
+            <h3>{title}</h3>
+            <h4>({year})</h4>
+            <p>{description}</p>
+          </div>
         </div>
-      </div>
+      </Link>
     )
   }
 })
