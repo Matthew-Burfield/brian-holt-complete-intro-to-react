640ca77ade030743aeaee507a9ff4e8d3ae45ee3
Matthew-Burfield <m1.burfield@gmail.com>
2017-04-24 10:21:19 +1000

    fixed Details styling bug with header

--- js/Details.js
+++ js/Details.js
@@ -16,20 +16,20 @@ const Details = React.createClass({
       <div className='details'>
         <header>
           <h1>svideo</h1>
-          <section>
-            <h1>{title}</h1>
-            <h2>{year}</h2>
-            <img src={`/public/img/posters/${poster}`} alt='poster' />
-            <p>{description}</p>
-          </section>
-          <div>
-            <iframe
-              src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
-              frameBorder='0'
-              allowFullScreen
-            />
-          </div>
         </header>
+        <section>
+          <h1>{title}</h1>
+          <h2>{year}</h2>
+          <img src={`/public/img/posters/${poster}`} />
+          <p>{description}</p>
+        </section>
+        <div>
+          <iframe
+            src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+            frameBorder='0'
+            allowFullScreen
+          />
+        </div>
       </div>
     )
   }
