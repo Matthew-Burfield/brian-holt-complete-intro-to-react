405c9a224292bd645fb7bf5a98eb2f95fc8518ea
Matthew-Burfield <m1.burfield@gmail.com>
2017-04-24 13:46:26 +1000

    Allowed Search compenent to get it's searchTerm from the redux store via mapStateToProps and connect

--- js/Landing.js
+++ js/Landing.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
+import { setSearchTerm } from './actionCreators'
 
-const { string } = React.PropTypes
+const { string, func, object } = React.PropTypes
 
 const Landing = React.createClass({
+  contextTypes: {
+    router: object
+  },
   propTypes: {
-    searchTerm: string
+    searchTerm: string,
+    dispatch: func
+  },
+  handleSearchTermChange (event) {
+    this.props.dispatch(setSearchTerm(event.target.value))
+  },
+  handleSearchSubmit (event) {
+    event.preventDefault()
+    this.context.router.transitionTo('/search') // Programatically navigate with react router
   },
   render () {
     return (
       <div className='landing'>
         <h1>svideo</h1>
-        <input value={this.props.searchTerm} type='text' placeholder='search' />
+        <input
+          onChange={this.handleSearchTermChange}
+          value={this.props.searchTerm}
+          type='text'
+          placeholder='search'
+        />
         <Link to='/search'>or Browse All</Link>
       </div>
     )
